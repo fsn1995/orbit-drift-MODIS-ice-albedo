963f3daa58355d995da4f5f0108f3e242c28d7a6
fsn1995 <fsn.1995@gmail.com>
2022-10-02 09:35:46 +0000

    track position on matlab

--- celestrak/sattrack.m
+++ celestrak/sattrack.m
@@ -1,13 +1,34 @@
-
-sc = satelliteScenario;
+%%
+startTime = datetime(2002,3,1);
+stopTIme  = datetime(2022,9,30);
+timeline = startTime:seconds(60):stopTIme;
+[y, m, d] = ymd(timeline);
+%%
+sc = satelliteScenario(startTime, stopTIme, 60);
 
 sat = satellite(sc, "sat000025994tle.txt");
 
-leadTime = 2*24*3600;                                          % seconds
-trailTime = leadTime;
-gt = groundTrack(sat,"LeadTime",leadTime,"TrailTime",trailTime)
+% leadTime = 2*24*3600;                                          % seconds
+% trailTime = leadTime;
+% gt = groundTrack(sat,"LeadTime",leadTime,"TrailTime",trailTime)
 
-satelliteScenarioViewer(sc, "Dimension","2D")
+% satelliteScenarioViewer(sc)
 
-time = datetime("2022-06-22");
-[position,velocity] = states(sat,time,"CoordinateFrame","geographic");
+position = states(sat,"CoordinateFrame","geographic");
+lat = position(1,:);
+lon = position(2,:);
+alt = position(3,:);
+
+%%
+summerSolstice = datetime(2002, 6, 22):years(1):datetime(2022, 6, 22);
+
+
+for i=1:length(summerSolstice)
+    indexTIme = summerSolstice(i);
+    [yi, mi, di] = ymd(indexTIme);
+    index = y==yi & m ==mi & d == di;
+    figure;
+    geoscatter(lat(index), lon(index));
+    title(string(indexTIme));
+end
+
